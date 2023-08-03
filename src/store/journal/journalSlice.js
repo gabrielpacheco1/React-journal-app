def5c6099a4c35d25c57eb4029625cfc08ffc2fa
Gabriel <cgabrielpacheco97@gmail.com>
2023-08-03 15:41:07 -0300

    reducers

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -7,19 +7,42 @@ export const journalSlice = createSlice({
     messageSaved: '',
     notes: [],
     // active: null,
-    active: {
-      id: 'abc12',
-      title: '',
-      body: '',
-      date: 1234,
-      imageUrls: [], //'https://img1.jpg', 'https://img2.jpg'
-    }
+    active: null,
+    // active: {
+    //   id: 'abc12',
+    //   title: '',
+    //   body: '',
+    //   date: 1234,
+    //   imageUrls: [], //'https://img1.jpg', 'https://img2.jpg'
+    // }
   },
   reducers: {
     addNewEmptyNote: (state, action) => {
       
     },
+    setActiveNote: (state, action) => {
+      
+    },
+    setNotes: (state, action) => {
+      
+    },
+    setSaving: (state, action) => {
+      
+    },
+    deleteNote: (state, action) => {
+      
+    },
+    deleteNoteById: (state, action) => {
+      
+    },
   },
 })
 
-export const { addNewEmptyNote } = journalSlice.actions
+export const { 
+  addNewEmptyNote,
+  setActiveNote,
+  setNotes,
+  setSaving,
+  deleteNote,
+  deleteNoteById, 
+} = journalSlice.actions
